604985efffc6431221f9455fd204301fa0a40970
Ashutosh Praharaj <ashutoshpraharaj01@gmail.com>
2024-12-12 16:35:52 +0530

    added method check answer

--- quiz_brain.py
+++ quiz_brain.py
@@ -11,19 +11,20 @@ class QuizBrain:
         if self.question_number != len(self.question_list):
             self.next_question()
 
+    def check_answer(self,correct_ans,user_ans):
+        if correct_ans == user_ans:
+            self.score += 1
+            print("Correct")
+        else:
+            print("Incorrect")
+        print(f"The correct answer was: {correct_ans}")
+        self.get_score()
+
     def next_question(self):
         current_question = self.question_list[self.question_number].text
         current_answer = self.question_list[self.question_number].answer
         user_input = input(f"Q.{self.question_number+1}: {current_question} (True/False)?: ")
         self.question_number +=1
-        if user_input == current_answer:
-            print("Correct")
-            print(f"The correct answer was: {current_answer}")
-            self.score +=1
-            self.get_score()
-        else:
-            print("Incorrect")
-            print(f"The correct answer was: {current_answer}")
-            self.get_score()
 
+        self.check_answer(correct_ans=current_answer,user_ans=user_input)
         self.still_has_questions()
